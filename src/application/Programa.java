99342b4ef4cedf983a0c22a094616510c8d88828
LucasTavares <lulucas25@hotmail.com>
2024-08-01 01:30:00 -0300

    feat:Fist class: position

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Olá  tavares");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
