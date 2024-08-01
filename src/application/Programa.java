f2bad8e15fdd69c1df242b369921b867f9cedaa3
LucasTavares <lulucas25@hotmail.com>
2024-08-01 03:02:38 -0300

    feat: chess layer and printing the board

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,12 +1,16 @@
 package application;
 
-import boardgame.Position;
+
+import chess.chessMath;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		
+		chessMath chessmath = new chessMath();
+		UI.printBoard(chessmath.getPieces());
+		
+		
 
 	}
 
